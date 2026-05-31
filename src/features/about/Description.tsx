c71f291ae555b95f091856806f2a973ee0b9658d
Felix Ferdinand <felixfdnd@gmail.com>
2026-05-31 22:14:36 +0700

    fix: 🐛 eslint

--- src/features/about/Description.tsx
+++ src/features/about/Description.tsx
@@ -1,15 +1,13 @@
 import styles from './about.module.scss';
 
 export default function Description() {
+  const description = `
+    Frontend Engineer with experience building scalable web applications using React.js and TypeScript.
+    I enjoy solving complex problems, improving development workflows, and collaborating with cross-functional teams to deliver impactful software solutions.
+
+    Beyond web development, I'm passionate about continuous learning and exploring new technologies, particularly in software architecture and Generative AI.
+    `;
   return (
-    <p className={styles.description}>
-      Frontend Engineer with experience building scalable web applications using React.js
-      and TypeScript. I enjoy solving complex problems, improving development workflows,
-      and collaborating with cross-functional teams to deliver impactful software solutions.
-      <br />
-      <br />
-      Beyond web development, I'm passionate about continuous learning and exploring new
-      technologies, particularly in software architecture and Generative AI.
-    </p>
+    <p className={styles.description}>{description}</p>
   );
 }
